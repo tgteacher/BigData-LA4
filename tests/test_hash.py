7fc5cbbba1dd23f23db70bccfa10e98348e7cdbd
Tristan Glatard <tristan.glatard@concordia.ca>
2018-03-27 23:36:55 -0400

    Various utility files

---
+++ tests/test_hash.py
@@ -0,0 +1,8 @@
+import subprocess, os
+
+def test_hash():
+    command="python ./answers/hash.py 123 12 7 99"
+    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    code=process.wait()
+    assert(not code), "Command failed"
+    assert(int(process.stdout.read().decode("utf-8"))==3)
