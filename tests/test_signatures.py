8fe51d35e4d3c218b0662dbf18862904b9d0ef45
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-23 16:02:51 -0400

    Fixes following input from @amrnablus

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -5,4 +5,6 @@ def test_signatures():
     process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     code=process.wait()
     assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8").strip()==open("tests/test-signatures.txt","r").read().strip())
+    output = process.stdout.read().decode("utf-8").strip()
+    assert(output==open("tests/test-signatures.txt","r").read().strip() or
+           output==open("tests/test-signatures-1.txt","r").read().strip())
