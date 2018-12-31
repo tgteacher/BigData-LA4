562cc8cb2c9c9b8e6d9ecd7fa8ab75bbf2e23ecd
valhayot <valeriehayot@gmail.com>
2018-12-30 21:16:22 -0500

    fixed bonus question tests

--- tests/test_b_and_r.py
+++ tests/test_b_and_r.py
@@ -1,8 +1,6 @@
-import subprocess, os
+from answers import get_b_and_r as br
+
 
 def test_b_and_r():
-    command="python ./answers/get_b_and_r.py 100 0.8"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/test-get-b-and-r.txt","r").read())
+    out = br(100, 0.8)
+    assert(out==open("tests/test-get-b-and-r.txt","r").read())
--- tests/test_b_and_r_undisclosed.py
+++ tests/test_b_and_r_undisclosed.py
@@ -1,8 +1,6 @@
-import subprocess, os
+from answers import get_b_and_r as br
+
 
 def test_b_and_r():
-    command="python ./answers/get_b_and_r.py 10 0.5"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/test-get-b-and-r-undisclosed.txt","r").read())
+    out = br(10, 0.5)
+    assert(out==open("tests/test-get-b-and-r-undisclosed.txt","r").read())
