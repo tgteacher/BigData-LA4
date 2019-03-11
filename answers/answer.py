b54f32fd61f655dc0f9a56e13b2fd9053c3d8017
valhayot <valeriehayot@gmail.com>
2019-03-10 21:57:55 -0400

    added all_states to skeleton

--- answers/answer.py
+++ answers/answer.py
@@ -7,6 +7,15 @@ import dask.bag as db
 import dask.dataframe as df
 
 
+all_states = ["ab", "ak", "ar", "az", "ca", "co", "ct", "de", "dc",
+              "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la",
+              "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv",
+              "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa",
+              "pr", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "vi",
+              "wa", "wv", "wi", "wy", "al", "bc", "mb", "nb", "lb", "nf",
+              "nt", "ns", "nu", "on", "qc", "sk", "yt", "dengl", "fraspm"]
+
+
 def init_spark():
     spark = SparkSession \
         .builder \
